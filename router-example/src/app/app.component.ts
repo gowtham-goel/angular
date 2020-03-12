a0458334826cb4155a9f0c35c46cd526b6428989
gowtham-goel <goelgowtham@gmail.com>
2020-03-13 01:47:44 +0530

    login status changes

--- router-example/src/app/app.component.ts
+++ router-example/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'router-example';
+  isLoggedIn = false;
+
+  constructor(private routes: Router) {}
+
+  ngOnInit() {
+    if (localStorage.getItem('username')) {
+      this.isLoggedIn = true;
+    }
+  }
+
+  logout() {
+    localStorage.removeItem('username');
+    this.routes.navigate(['/login']);
+    alert('logged out successfully');
+  }
 }
