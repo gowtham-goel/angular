564a98b11ddb2fa27317b354e9d2a849dd5cb1ae
gowtham-goel <goelgowtham@gmail.com>
2020-03-15 16:19:48 +0530

    drag example added

--- router-example/src/app/app-routing.module.ts
+++ router-example/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { SwaggerComponent } from './components/swagger/swagger.component';
+import { DragComponent } from './components/drag/drag.component';
 
 import { AuthGuard } from './guards/auth.guard';
 
@@ -11,7 +12,8 @@ const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
   {path: 'swagger', component: SwaggerComponent},
-  {path: '**', redirectTo: 'swagger'}
+  {path: 'drag', component: DragComponent},
+  {path: '**', redirectTo: 'drag'}
 ];
 
 @NgModule({
--- router-example/src/app/app.module.ts
+++ router-example/src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// custom modules
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { SwaggerComponent } from './components/swagger/swagger.component';
+import { DragComponent } from './components/drag/drag.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     DashboardComponent,
-    SwaggerComponent
+    SwaggerComponent,
+    DragComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
